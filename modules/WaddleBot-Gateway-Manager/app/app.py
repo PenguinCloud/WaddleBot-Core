f93d1df549917e061cc292c99564d755203239f7
thysw95 <thys103@gmail.com>
2024-07-08 14:47:17 +0200

    Added the WaddleBot Gateway Manager Module to the modules folder.
    Updated the listener readme file

---
+++ modules/WaddleBot-Gateway-Manager/app/app.py
@@ -0,0 +1,22 @@
+from flask import Flask
+from flask_restx import Resource, Api, fields, reqparse, abort
+import logging
+import json
+import os
+from datetime import datetime
+import base64
+import sys
+import requests
+
+from namespaces.gateway_creator_namespace import gateway_creator_namespace
+# from message_sender.twitch_msg import Twitch_Message_Sender
+
+app = Flask(__name__)
+
+api = Api(app, version='1.0', title='WADDLEBOT GATEWAY CREATION API', description='API for the creation of gateways for waddlebot', validate=True)
+
+api.add_namespace(gateway_creator_namespace)
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0', port=5000)
+
