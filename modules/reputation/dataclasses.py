045d83e917c56af01da933ba3009d27548b93fcd
Justin B <justin@penguintech.io>
2024-07-16 13:15:59 +0000

    Started and was interrupted by BCI

---
+++ modules/reputation/dataclasses.py
@@ -0,0 +1,17 @@
+from dataclasses import dataclass
+@dataclass
+class dbc:
+    database: str
+    dbhost: str
+    dbport: int
+    dbuser: str
+    dbpassword: str
+
+
+@dataclass
+class webdb:
+    webhost: str
+    webport: int
+    webuser: str
+    webpassword: str
+    database: str
--- modules/reputation/main.py
+++ modules/reputation/main.py
@@ -1,1 +1,12 @@
 #!/usr/bin/python3
+from dataclasses.py import dbc, webdb
+
+class reputation:
+    def __init__(self) -> None:
+        self.score = 0
+        self.userid = None
+        self.platform = None
+        self.interface = None
+        self.subinterface = None
+        self.database = dbc
+        self.webdb = webdb
