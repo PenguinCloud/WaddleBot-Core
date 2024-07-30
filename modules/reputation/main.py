12fd45077f1ab18ac96c919a2ad82b626674bf75
Justin B <justin@penguintech.io>
2024-07-30 01:53:20 +0000

    further class definement

--- modules/reputation/main.py
+++ modules/reputation/main.py
@@ -14,4 +14,16 @@ class twitch:
         self.eventRoute = eventRoute
         self.event = event
 
+class discord:
+    def __init__(self, event: str, eventEntity: entity, eventRoute: route ) -> None:
+        self.score = 0
+        self.userid = None
+        self.platform = None
+        self.interface = None
+        self.subinterface = None
+        self.database = dbc
+        self.webdb = webdb
+        self.eventEntity = eventEntity
+        self.eventRoute = eventRoute
+        self.event = event
 
