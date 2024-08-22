711590720f9a328e118942e3276ba20163d542dc
thysw95 <thys103@gmail.com>
2024-08-22 08:40:48 +0200

    The main listener module now retrieves the necessary URLS and env variables from the environment

--- modules/WaddleBot-Listener/listener_main.py
+++ modules/WaddleBot-Listener/listener_main.py
@@ -1,23 +1,28 @@
 from src.listener.listener import WaddleBotListener
+from dotenv import load_dotenv
+import os
+
+# Load the environment variables
+load_dotenv()
 
 # Matterbridge API URL to manage messages
-matterbridgeURL = 'http://localhost:4200/api/'
+matterbridgeURL = os.getenv('MATTERBRIDGE_URL')
 
 # User manager API URL to add users to the database
-userManagerURL = 'http://localhost:8000/WaddleDBM/identities/create.json/'
+userManagerURL = os.getenv('USER_MANAGER_URL')
 
 # Marketplace API URL to manage the marketplace
-marketplaceURL = 'http://localhost:8000/marketplace_manager/marketplace/get.json'
+marketplaceURL = os.getenv('MARKETPLACE_URL')
 
 # Community Modules API URL to manage community modules to get the community modules
-communityModulesURL = 'http://localhost:8000/WaddleDBM/community_modules/get_by_community_name_and_module_id.json/'
+communityModulesURL = os.getenv('COMMUNITY_MODULES_URL')
 
 # Initial context API URL to set the initial context of new users to the database.
-contextURL = 'http://localhost:8000/WaddleDBM/context/'
+contextURL = os.getenv('CONTEXT_URL')
 
 # Redis parameters
-redisHost = 'localhost'
-redisPort = 6379
+redisHost = os.getenv('REDIS_HOST')
+redisPort = os.getenv('REDIS_PORT')
 
 # The main function of the program
 def main() -> None:
